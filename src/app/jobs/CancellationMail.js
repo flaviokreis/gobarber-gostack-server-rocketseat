455377edd3a132e64c8b1d969730be204d476d4e
Flavio Kreis <flaviokreis@gmail.com>
2019-10-24 14:03:17 -0300

    GoBarber part 1, 2 and 3

---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,29 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  get key() {
+    return 'CancellationMail';
+  }
+
+  async handle({ data }) {
+    const { appointment } = data;
+
+    Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agentamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(parseISO(appointment.date), "dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt
+        })
+      }
+    });
+  }
+}
+
+export default new CancellationMail();
